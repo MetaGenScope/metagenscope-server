68f261052e31cee90f8ff9068a317d5030df015d
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2017-11-25 12:17:40 -0500

    Added Organizations model.

---
+++ app/tests/test_organization_model.py
@@ -0,0 +1,31 @@
+"""Test suite for Organization model."""
+
+from sqlalchemy.exc import IntegrityError
+
+from app import db
+from app.api.models import User, Organization
+from app.tests.base import BaseTestCase
+from app.tests.utils import add_user, add_organization
+
+
+class TestOrganizationModel(BaseTestCase):
+    """Test suite for Organization model."""
+
+    def test_add_organization(self):
+        """Ensure organization model is created correctly."""
+        organization = add_organization('Test Organization', 'admin@test.org')
+        self.assertTrue(organization.id)
+        self.assertEqual(organization.name, 'Test Organization')
+        self.assertEqual(organization.adminEmail, 'admin@test.org')
+        self.assertTrue(organization.created_at)
+
+    # pylint: disable=invalid-name
+    def test_add_organziation_duplicate_name(self):
+        """Ensure duplicate names are not allowed."""
+        add_organization('Test Organization', 'admin@test.org')
+        duplicate_organization = Organization(
+            name='Test Organization',
+            adminEmail='test@test2.org',
+        )
+        db.session.add(duplicate_organization)
+        self.assertRaises(IntegrityError, db.session.commit)
--- app/tests/utils.py
+++ app/tests/utils.py
@@ -4,7 +4,7 @@ import datetime
 
 
 from app import db
-from app.api.models import User
+from app.api.models import User, Organization
 
 
 def add_user(username, email, password, created_at=datetime.datetime.utcnow()):
@@ -17,3 +17,11 @@ def add_user(username, email, password, created_at=datetime.datetime.utcnow()):
     db.session.add(user)
     db.session.commit()
     return user
+
+
+def add_organization(name, admin_email, created_at=datetime.datetime.utcnow()):
+    """Wrap functionality for adding organization."""
+    organization = Organization(name=name, adminEmail=admin_email, created_at=created_at)
+    db.session.add(organization)
+    db.session.commit()
+    return organization
