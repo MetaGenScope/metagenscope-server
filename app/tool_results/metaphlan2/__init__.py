158bf310405d5cee465d15ddd861e0b5c8418f2f
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-03-02 10:18:36 -0500

    Add Metaphlan 2 tests.

--- app/tool_results/metaphlan2/__init__.py
+++ app/tool_results/metaphlan2/__init__.py
@@ -7,7 +7,7 @@ from app.tool_results.tool_module import ToolResult, ToolResultModule
 class Metaphlan2Result(ToolResult):     # pylint: disable=too-few-public-methods
     """Metaphlan 2 tool's result type."""
 
-    # The taxa dict is a map from taxon name to abundance value
+    # Taxa is of the form: {<taxon_name>: <abundance_value>}
     taxa = mongoDB.DictField()
 
 
---
+++ app/tool_results/metaphlan2/tests/test_metaphlan2_upload.py
@@ -0,0 +1,39 @@
+"""Test suite for Metaphlan 2 tool result uploads."""
+
+import json
+
+from app.samples.sample_models import Sample
+from app.tool_results.metaphlan2.tests.constants import TEST_TAXA
+from app.api.utils import uuid2slug
+from tests.base import BaseTestCase
+from tests.utils import with_user
+
+
+class TestMetaphlan2Uploads(BaseTestCase):
+    """Test suite for Metaphlan 2 tool result uploads."""
+
+    @with_user
+    def test_upload_metaphlan2(self, auth_headers, *_):
+        """Ensure a raw Metaphlan 2 tool result can be uploaded."""
+        sample = Sample(name='SMPL_Metaphlan_01').save()
+        sample_uuid = sample.uuid
+        sample_slug = uuid2slug(sample_uuid)
+        with self.client:
+            response = self.client.post(
+                f'/api/v1/samples/{sample_slug}/metaphlan2',
+                headers=auth_headers,
+                data=json.dumps(dict(
+                    taxa=TEST_TAXA,
+                )),
+                content_type='application/json',
+            )
+            # Ensure response contains Metaphlan data
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('taxa', data['data'])
+            self.assertEqual(data['data']['taxa']['d__Viruses'], 1733)
+            self.assertIn('success', data['status'])
+
+        # Reload object to ensure Metaphlan 2 result was stored properly
+        sample = Sample.objects(uuid=sample_uuid)[0]
+        self.assertTrue(sample.metaphlan2)
