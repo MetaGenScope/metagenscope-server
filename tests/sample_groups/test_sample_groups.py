6cb45fbb70a93be23bf4a7f9b254689d3e43ea66
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-03-08 14:09:40 -0500

    Add test for SampleGroup->Samples association.

--- tests/sample_groups/test_sample_groups.py
+++ tests/sample_groups/test_sample_groups.py
@@ -3,13 +3,14 @@
 from sqlalchemy.exc import IntegrityError
 
 from app import db
+from app.samples.sample_models import Sample
 from app.sample_groups.sample_group_models import SampleGroup
 from tests.base import BaseTestCase
 from tests.utils import add_sample_group
 
 
-class TestUserModel(BaseTestCase):
-    """Test suite for User model."""
+class TestSampleGroupModel(BaseTestCase):
+    """Test suite for SampleGroup model."""
 
     def test_add_sample_group(self):
         """Ensure sample group model is created correctly."""
@@ -28,3 +29,18 @@ class TestUserModel(BaseTestCase):
         )
         db.session.add(duplicate_group)
         self.assertRaises(IntegrityError, db.session.commit)
+
+    def test_add_samples(self):
+        """Ensure that samples can be added to SampleGroup."""
+        sample_group = add_sample_group('Sample Group One', 'public')
+        sample_one = Sample(name='SMPL_01', metadata={'subject_group': 1}).save()
+        sample_two = Sample(name='SMPL_02', metadata={'subject_group': 4}).save()
+        sample_group.samples = [sample_one, sample_two]
+        db.session.commit()
+        self.assertEqual(len(sample_group.sample_ids), 2)
+        self.assertIn(sample_one.uuid, sample_group.sample_ids)
+        self.assertIn(sample_two.uuid, sample_group.sample_ids)
+        samples = sample_group.samples
+        self.assertEqual(len(samples), 2)
+        self.assertIn(sample_one, samples)
+        self.assertIn(sample_two, samples)
