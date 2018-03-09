2e1a826c9dd861b64f3bb0c9d874322894a9efb9
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-03-09 17:46:57 -0500

    Add test for run_tsne().

--- app/display_modules/sample_similarity/tests/test_sample_similarity_tasks.py
+++ app/display_modules/sample_similarity/tests/test_sample_similarity_tasks.py
@@ -1,6 +1,9 @@
 """Test suite for Sample Similarity tasks."""
 
-from app.display_modules.sample_similarity.sample_similarity_tasks import get_clean_samples
+from app.display_modules.sample_similarity.sample_similarity_tasks import (
+    get_clean_samples,
+    run_tsne,
+)
 from app.tool_results.kraken.tests.kraken_factory import create_kraken
 
 from tests.base import BaseTestCase
@@ -10,7 +13,7 @@ class TestSampleSimilarityTasks(BaseTestCase):
     """Test suite for Sample Similarity tasks."""
 
     def test_clean_samples(self):
-        """Ensure clean_samples method adds missing features to all samples."""
+        """Ensure get_clean_samples method adds missing features to all samples."""
         sample_dict = {f'SMPL_{i}': create_kraken().taxa for i in range(3)}
 
         all_feature_ids = set([])
@@ -24,7 +27,7 @@ class TestSampleSimilarityTasks(BaseTestCase):
                 self.assertIn(feature_id, feature_set)
 
     def test_clean_zeroed_samples(self):
-        """Ensure clean_samples method removes features below threshold."""
+        """Ensure get_clean_samples method removes features below threshold."""
         sample_dict = {f'SMPL_{i}': dict(create_kraken().taxa) for i in range(3)}
         sample_dict['SMPL_1']['somebadkingdom'] = 0.0000001
 
@@ -36,3 +39,9 @@ class TestSampleSimilarityTasks(BaseTestCase):
 
         for feature_set in result.values():
             self.assertNotIn('somebadkingdom', feature_set)
+
+    def test_tsne_returns_data(self):
+        """Ensure run_tsne method removes features below threshold."""
+        sample_dict = {f'SMPL_{i}': dict(create_kraken().taxa) for i in range(3)}
+        tsne_output = run_tsne(sample_dict)
+        self.assertEqual((3, 2), tsne_output.shape)
