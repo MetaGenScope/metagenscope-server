379eaaa6fe6beedbb1004f7cc59f0036c8eedc15
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-03-16 00:35:03 -0400

    Remove unused import. [skip ci]

--- app/display_modules/__init__.py
+++ app/display_modules/__init__.py
@@ -1,7 +1,5 @@
 """Modules for converting analysis tool output to front-end display data."""
 
-from app.extensions import mongoDB
-
 from app.display_modules.hmp import HMPModule
 from app.display_modules.reads_classified import ReadsClassifiedModule
 from app.display_modules.sample_similarity import SampleSimilarityDisplayModule
