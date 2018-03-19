7f8281b928a2596f11cf549377c347af36a69cb4
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-03-19 16:11:49 -0400

    Add tests for model and tasks.

---
+++ app/display_modules/microbe_directory/tests/test_tasks.py
@@ -0,0 +1,9 @@
+"""Test suite for Microbe Directory tasks."""
+
+from tests.base import BaseTestCase
+
+
+class TestMicrobeDirectoryTasks(BaseTestCase):
+    """Test suite for Microbe Directory tasks."""
+
+    # Stub - Microbe Directory does not have validation rules to check
