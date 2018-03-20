f054a6a6070a3aa9eda04533505244c60fa41805
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-03-20 12:57:08 -0400

    Add set_module_status().

--- app/display_modules/microbe_directory/wrangler.py
+++ app/display_modules/microbe_directory/wrangler.py
@@ -2,7 +2,6 @@
 
 from celery import chain
 
-from app.analysis_results.analysis_result_models import AnalysisResultWrapper
 from app.display_modules.display_wrangler import DisplayModuleWrangler
 from app.display_modules.utils import persist_result, collate_samples
 from app.sample_groups.sample_group_models import SampleGroup
@@ -22,18 +21,13 @@ class MicrobeDirectoryWrangler(DisplayModuleWrangler):
     def run_sample_group(cls, sample_group_id):
         """Gather and process samples."""
         sample_group = SampleGroup.query.filter_by(id=sample_group_id).first()
-
-        # Set state on Analysis Group
-        analysis_group = sample_group.analysis_result
-        wrapper = AnalysisResultWrapper(status='W')
-        setattr(analysis_group, MODULE_NAME, wrapper)
-        analysis_group.save()
+        sample_group.set_module_status(MODULE_NAME, 'W')
 
         tool_result_name = MicrobeDirectoryResultModule.name()
         collate_fields = MicrobeDirectoryToolResult._fields
         collate_task = collate_samples.s(tool_result_name, collate_fields, sample_group_id)
         reducer_task = microbe_directory_reducer.s()
-        persist_task = persist_result.s(analysis_group.uuid, MODULE_NAME)
+        persist_task = persist_result.s(sample_group.analysis_result_uuid, MODULE_NAME)
 
         task_chain = chain(collate_task, reducer_task, persist_task)
         result = task_chain.delay()
--- app/display_modules/read_stats/wrangler.py
+++ app/display_modules/read_stats/wrangler.py
@@ -1,6 +1,7 @@
 """Read Stats wrangler and related."""
 
-from app.analysis_results.analysis_result_models import AnalysisResultWrapper
+from celery import chain
+
 from app.extensions import mongoDB as mdb
 from app.display_modules.display_wrangler import DisplayModuleWrangler
 from app.display_modules.utils import persist_result, collate_samples
@@ -25,16 +26,11 @@ class ReadStatsWrangler(DisplayModuleWrangler):
     def run_sample_group(cls, sample_group_id):
         """Gather and process samples."""
         sample_group = SampleGroup.query.filter_by(id=sample_group_id).first()
+        sample_group.set_module_status(sample_group, MODULE_NAME, 'W')
 
-        # Set state on Analysis Group
-        analysis_group = sample_group.analysis_result
-        wrapper = AnalysisResultWrapper(status='W')
-        setattr(analysis_group, MODULE_NAME, wrapper)
-        analysis_group.save()
+        persist_task = persist_result.s(sample_group.analysis_group_uuid, MODULE_NAME)
 
-        persist_task = persist_result.s(analysis_group.uuid, MODULE_NAME)
-
-        task_chain = (cls.collate_task.s(sample_group.samples) | persist_task)
-        result = task_chain().get()
+        task_chain = chain(cls.collate_task.s(sample_group.samples), persist_task)
+        result = task_chain().delay()
 
         return result
