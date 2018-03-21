7f8c86943b58a26dc3d84a1ef304f2d422c02439
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-03-21 14:30:25 -0400

    Add vfdb tool result.

---
+++ app/tool_results/vfdb/models.py
@@ -0,0 +1,19 @@
+"""Models for Virulence Factor tool module."""
+
+from app.extensions import mongoDB
+from app.tool_results.tool_module import ToolResult
+
+
+class VFDBRow(mongoDB.EmbeddedDocument):  # pylint: disable=too-few-public-methods
+    """Row for a gene in Methyltransferase."""
+
+    rpk = mongoDB.FloatField()
+    rpkm = mongoDB.FloatField()
+    rpkmg = mongoDB.FloatField()
+
+
+class VFDBToolResult(ToolResult):  # pylint: disable=too-few-public-methods
+    """Virulence Factor result type."""
+
+    vfdb_row_field = mongoDB.EmbeddedDocumentField(VFDBRow)
+    genes = mongoDB.MapField(field=vfdb_row_field, required=True)
