6966bd20d22349ed3691180fd9f4886151612bb7
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-03-21 14:03:49 -0400

    Add Methyls display module.

---
+++ app/display_modules/methyls/__init__.py
@@ -0,0 +1,32 @@
+"""Methyls module."""
+
+from app.display_modules.display_module import DisplayModule
+from app.tool_results.methyltransferases import MethylResultModule
+
+from .constants import MODULE_NAME
+from .models import MethylResult
+from .wrangler import MethylWrangler
+
+
+class MethylsDisplayModule(DisplayModule):
+    """Methyltransferase display module."""
+
+    @staticmethod
+    def required_tool_results():
+        """Return a list of the necessary result modules."""
+        return [MethylResultModule]
+
+    @classmethod
+    def name(cls):
+        """Return the name of the module."""
+        return MODULE_NAME
+
+    @classmethod
+    def get_result_model(cls):
+        """Return the embedded result."""
+        return MethylResult
+
+    @classmethod
+    def get_wrangler(cls):
+        """Return the wrangler class."""
+        return MethylWrangler
---
+++ app/display_modules/methyls/tasks.py
@@ -0,0 +1,49 @@
+"""Tasks for generating Methyl results."""
+
+import numpy as np
+import pandas as pd
+
+from app.extensions import celery
+from app.tool_results.methyltransferases import MethylResultModule
+
+from .constants import TOP_N
+
+
+def fill_gene_array(gene_array, gene_names):
+    """Fill in missing gene names in gene_array with 0."""
+    out = {}
+    for gene_name in gene_names:
+        try:
+            out[gene_name] = gene_array[gene_names]
+        except KeyError:
+            out[gene_name] = 0
+    return out
+
+
+def transform_sample(sample_vals, gene_names):
+    """Transform sample values to rpkm output."""
+    out = {
+        'rpkm': fill_gene_array(sample_vals.rpkm, gene_names),
+        'rpkmg': fill_gene_array(sample_vals.rpkmg, gene_names),
+    }
+    return out
+
+
+@celery.task()
+def filter_methyl_results(samples):
+    """Reduce Methyl results to the <TOP_N> mean abundance genes (rpkm)."""
+    sample_dict = {sample.name: getattr(sample, MethylResultModule.name())
+                   for sample in samples}
+    rpkm_dict = {sname: vfdb.rpkm for sname, vfdb in sample_dict.items()}
+
+    # Columns are samples, rows are genes, vals are rpkms
+    rpkm_tbl = pd.DataFrame(rpkm_dict).fillna(0)
+    rpkm_mean = np.array(rpkm_tbl.mean(axis=0))
+
+    idx = (-1 * rpkm_mean).argsort()[:TOP_N]
+    gene_names = set(rpkm_tbl.index.iloc[idx])
+
+    filtered_sample_tbl = {sname: transform_sample(vfdb, gene_names)
+                           for sname, vfdb in sample_dict.items()}
+
+    return {'samples': filtered_sample_tbl}
