bf24e1aa8b2c4389f02c384eeb01717537c8c869
David Danko <dcdanko@gmail.com>
2018-03-30 13:42:38 -0400

    tool result tests

---
+++ app/tool_results/vfdb/tests/factory.py
@@ -0,0 +1,30 @@
+"""Factory for generating Kraken result models for testing."""
+
+from random import randint
+
+from app.tool_results.vfdb import VFDBToolResult
+
+
+def simulate_gene():
+    """Return one row."""
+    gene_name = 'sample_vfdb_gene_{}'.format(randint(1, 100))
+    rpk = randint(1, 1000) / 0.33333
+    rpkm = randint(1, 1000) / 0.33333
+    rpkmg = randint(1, 1000) / 0.33333
+    return gene_name, {'rpk': rpk, 'rpkm': rpkm, 'rpkmg': rpkmg}
+
+
+def create_values():
+    """Create methyl values."""
+    genes = [simulate_gene() for _ in range(randint(3, 10))]
+    result = {
+        'genes': {gene_name: row for gene_name, row in genes}
+
+    }
+    return result
+
+
+def create_methyls():
+    """Create VFDBlToolResult with randomized field data."""
+    packed_data = create_values()
+    return VFDBToolResult(**packed_data)
---
+++ app/tool_results/vfdb/tests/test_module.py
@@ -0,0 +1,45 @@
+"""Test suite for VFDB tool result model."""
+import json
+
+from app.samples.sample_models import Sample
+from app.tool_results.methyltransferases import VFDBToolResult
+
+from tests.base import BaseTestCase
+from tests.utils import with_user
+
+from .factory import create_values
+
+
+class TestMethylsModel(BaseTestCase):
+    """Test suite for VFDB tool result model."""
+
+    def test_add_methyls(self):
+        """Ensure VFDB tool result model is created correctly."""
+
+        methyls = VFDBToolResult(**create_values())
+        sample = Sample(name='SMPL_01',
+                        vfdb_quantify=methyls).save()
+        self.assertTrue(sample.vfdb_quantify)
+
+    @with_user
+    def test_upload_methyls(self, auth_headers, *_):
+        """Ensure a raw Methyl tool result can be uploaded."""
+        sample = Sample(name='SMPL_Microbe_Directory_01').save()
+        sample_uuid = str(sample.uuid)
+        vals = create_values()
+        with self.client:
+            response = self.client.post(
+                f'/api/v1/samples/{sample_uuid}/vfdb_quantify',
+                headers=auth_headers,
+                data=json.dumps(vals),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('success', data['status'])
+            for field in vals:
+                self.assertIn(field, data['data'])
+
+        # Reload object to ensure microbe directory result was stored properly
+        sample = Sample.objects.get(uuid=sample_uuid)
+        self.assertTrue(sample.vfdb_quantify)
