ddeb3ff9284186f53b597d7b636e242b7d8b4ff9
David Danko <dcdanko@gmail.com>
2018-03-30 12:31:57 -0400

    generic display module test

--- app/display_modules/methyls/tests/test_module.py
+++ app/display_modules/methyls/tests/test_module.py
@@ -1,41 +1,22 @@
 """Test suite for Methyls diplay module."""
-import json
-
-from app import db
+from app.display_modules.display_module_base_test import BaseDisplayModuleTest
 from app.display_modules.methyls.wrangler import MethylWrangler
 from app.samples.sample_models import Sample
-from app.analysis_results.analysis_result_models import (
-    AnalysisResultMeta,
-    AnalysisResultWrapper
-)
 from app.display_modules.methyls import MethylResult
 from app.display_modules.methyls.tests.factory import (
     MethylsFactory,
     create_one_sample
 )
 from app.tool_results.methyltransferases.tests.factory import create_methyls
-from tests.base import BaseTestCase
-from tests.utils import add_sample_group
 
 
-class TestMethylsModule(BaseTestCase):
+class TestMethylsModule(BaseDisplayModuleTest):
     """Test suite for Methyls diplay module."""
 
     def test_get_methyls(self):
         """Ensure getting a single Methyl behaves correctly."""
         methyls = MethylsFactory()
-        wrapper = AnalysisResultWrapper(data=methyls, status='S')
-        analysis_result = AnalysisResultMeta(methyltransferases=wrapper).save()
-        with self.client:
-            response = self.client.get(
-                f'/api/v1/analysis_results/{analysis_result.uuid}/methyltransferases',
-                content_type='application/json',
-            )
-            data = json.loads(response.data.decode())
-            self.assertEqual(response.status_code, 200)
-            self.assertIn('success', data['status'])
-            self.assertEqual(data['data']['status'], 'S')
-            self.assertIn('samples', data['data']['data'])
+        self.generic_getter_test(methyls, 'methyltransferases')
 
     def test_add_methyls(self):
         """Ensure Methyl model is created correctly."""
@@ -44,27 +25,17 @@ class TestMethylsModule(BaseTestCase):
             'test_sample_2': create_one_sample()
         }
         methyls_result = MethylResult(samples=samples)
-        wrapper = AnalysisResultWrapper(data=methyls_result)
-        result = AnalysisResultMeta(methyltransferases=wrapper).save()
-        self.assertTrue(result.uuid)
-        self.assertTrue(result.methyltransferases)
+        self.generic_adder_test(methyls_result, 'methyltransferases')
 
     def test_run_methyls_sample_group(self):  # pylint: disable=invalid-name
         """Ensure methyls run_sample_group produces correct results."""
 
         def create_sample(i):
             """Create unique sample for index i."""
-            metadata = {'foobar': f'baz{i}'}
-            data = create_methyls()
             return Sample(name=f'Sample{i}',
-                          metadata=metadata,
-                          methyltransferases=data).save()
+                          metadata={'foobar': f'baz{i}'},
+                          methyltransferases=create_methyls()).save()
 
-        sample_group = add_sample_group(name='SampleGroup01')
-        sample_group.samples = [create_sample(i) for i in range(6)]
-        db.session.commit()
-        MethylWrangler.run_sample_group(sample_group.id).get()
-        analysis_result = sample_group.analysis_result
-        self.assertIn('methyltransferases', analysis_result)
-        methyls = analysis_result.methyltransferases
-        self.assertEqual(methyls.status, 'S')
+        self.generic_run_group_test(create_sample,
+                                    MethylWrangler,
+                                    'methyltransferases')
