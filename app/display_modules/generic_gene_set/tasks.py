6dd7b5ed47cd871f8e9287db8089521b1b0a0f0a
David Danko <dcdanko@gmail.com>
2018-04-02 11:08:19 -0400

    reduced local variables

--- app/display_modules/generic_gene_set/tasks.py
+++ app/display_modules/generic_gene_set/tasks.py
@@ -20,11 +20,7 @@ def transform_sample(vfdb_tool_result, gene_names):
     return out
 
 
-@celery.task()
-def filter_gene_results(samples, tool_result_name, result_type, top_n):  # pylint disable=too-many-locals
-    """Reduce Methyl results to the <top_n> mean abundance genes (rpkm)."""
-    sample_dict = {sample.name: getattr(sample, tool_result_name)
-                   for sample in samples}
+def get_rpkm_tbl(sample_dict):
     rpkm_dict = {}
     for sname, methyl_tool_result in sample_dict.items():
         rpkm_dict[sname] = {}
@@ -34,9 +30,23 @@ def filter_gene_results(samples, tool_result_name, result_type, top_n):  # pylin
     # Columns are samples, rows are genes, vals are rpkms
     rpkm_tbl = pd.DataFrame(rpkm_dict).fillna(0)
     rpkm_mean = np.array(rpkm_tbl.mean(axis=0))
+    return rpkm_tbl, rpkm_mean
 
+
+def get_top_genes(rpkm_tbl, rpkm_mean, top_n):
     idx = (-1 * rpkm_mean).argsort()[:top_n]
     gene_names = set(rpkm_tbl.index[idx])
+    return gene_names
+
+
+@celery.task()
+def filter_gene_results(samples, tool_result_name, result_type, top_n):
+    """Reduce Methyl results to the <top_n> mean abundance genes (rpkm)."""
+    sample_dict = {sample.name: getattr(sample, tool_result_name)
+                   for sample in samples}
+
+    rpkm_tbl, rpkm_mean = get_rpkm_tbl(sample_dict)
+    gene_names = get_top_genes(rpkm_tbl, rpkm_mean, top_n)
 
     filtered_sample_tbl = {sname: transform_sample(vfdb_tool_result, gene_names)
                            for sname, vfdb_tool_result in sample_dict.items()}
