c3557445a61e05ac2e3a21b6970387ac296ce452
David Danko <dcdanko@gmail.com>
2018-04-05 18:33:26 -0400

    added test suite for hmp display module

---
+++ app/display_modules/hmp/tests/factory.py
@@ -0,0 +1,56 @@
+# pylint: disable=missing-docstring,too-few-public-methods,no-self-use
+
+"""Factory for generating HMP models for testing."""
+
+from random import random, randint
+
+import factory
+
+from app.display_modules.hmp import HMPResult
+
+
+def fake_distribution():
+    """Return a random 'distribution'."""
+    distro = [random() for _ in range(5)]
+    return sorted(distro)
+
+
+def fake_categories():
+    """Return fake categories."""
+    out = {}
+    for cat_name in ['cat_1', 'cat_2']:
+        out[cat_name] = []
+        for i in range(randint(2, 4)):
+            out[cat_name].append(cat_name + str(i))
+    return out
+
+
+def fake_sites():
+    """Return fake sites."""
+    return ['skin', 'oral', 'urogenital', 'airways']
+
+
+class HMPFactory(factory.mongoengine.MongoEngineFactory):
+    """Factory for Analysis Result's HMP."""
+
+    class Meta:
+        """Factory metadata."""
+
+        model = HMPResult
+
+    @factory.lazy_attribute
+    def categories(self):
+        return fake_categories()
+
+    @factory.lazy_attribute
+    def sites(self):
+        return fake_sites()
+
+    @factory.lazy_attribute
+    def data(self):
+        out = {}
+        for cat_vals in self.categories.values():
+            for cat_val in cat_vals:
+                out[cat_val] = [{'name': site, 'data': fake_distribution()}
+                                for site in self.sites]
+        return out
---
+++ app/display_modules/hmp/tests/test_module.py
@@ -0,0 +1,55 @@
+"""Test suite for HMP model."""
+
+from mongoengine import ValidationError
+
+from app.analysis_results.analysis_result_models import AnalysisResultWrapper, AnalysisResultMeta
+from app.display_modules.display_module_base_test import BaseDisplayModuleTest
+from app.display_modules.hmp.wrangler import HMPWrangler
+from app.samples.sample_models import Sample
+from app.display_modules.hmp.models import HMPResult
+from app.display_modules.hmp.constants import MODULE_NAME
+from app.display_modules.hmp.tests.factory import HMPFactory
+from app.tool_results.hmp_sites.tests.factory import create_hmp_sites
+
+from .factory import (
+    HMPFactory,
+    fake_categories,
+    fake_sites
+)
+
+
+class TestHMPResult(BaseDisplayModuleTest):
+    """Test suite for HMP model."""
+
+    def test_get_hmp(self):
+        """Ensure getting a single HMP behaves correctly."""
+        hmp = HMPFactory()
+        self.generic_getter_test(hmp, MODULE_NAME)
+
+    def test_add_hmp(self):
+        """Ensure HMP model is created correctly."""
+        hmp = HMPFactory()
+        self.generic_adder_test(hmp, MODULE_NAME)
+
+    def test_add_missing_category(self):
+        """Ensure saving model fails if category is missing from `data`."""
+        hmp = HMPResult(categories=fake_categories(),
+                        sites=fake_sites(),
+                        data={})
+        wrapper = AnalysisResultWrapper(data=hmp)
+        result = AnalysisResultMeta(hmp=wrapper)
+        self.assertRaises(ValidationError, result.save)
+
+    def test_run_hmp_sample_group(self):  # pylint: disable=invalid-name
+        """Ensure hmp run_sample_group produces correct results."""
+
+        def create_sample(i):
+            """Create unique sample for index i."""
+            data = create_hmp_sites()
+            return Sample(name=f'Sample{i}',
+                          metadata={'foobar': f'baz{i}'},
+                          hmp_site_dists=data).save()
+
+        self.generic_run_group_test(create_sample,
+                                    HMPWrangler,
+                                    MODULE_NAME)
