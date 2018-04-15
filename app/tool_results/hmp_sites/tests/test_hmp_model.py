b418dfae0bd6f6327a70bdd80d433ec668588b0e
David Danko <dcdanko@gmail.com>
2018-04-15 17:05:45 -0400

    fixed hmp tests

--- app/tool_results/hmp_sites/tests/test_hmp_model.py
+++ app/tool_results/hmp_sites/tests/test_hmp_model.py
@@ -4,6 +4,7 @@ from mongoengine import ValidationError
 
 from app.samples.sample_models import Sample
 from app.tool_results.hmp_sites import HmpSitesResult
+from app.tool_results.hmp_sites.constants import MODULE_NAME
 from app.tool_results.hmp_sites.tests.constants import TEST_HMP
 
 from tests.base import BaseTestCase
@@ -15,7 +16,8 @@ class TestHmpSitesModel(BaseTestCase):
     def test_add_hmp_sites_result(self):
         """Ensure HMP Sites result model is created correctly."""
         hmp_sites = HmpSitesResult(**TEST_HMP)
-        sample = Sample(name='SMPL_01', hmp_sites=hmp_sites).save()
+        args = {'name': 'SMPL_01', MODULE_NAME: hmp_sites}
+        sample = Sample(**args).save()
         self.assertTrue(sample.hmp_sites)
         tool_result = sample.hmp_sites
         self.assertEqual(len(tool_result), 5)
@@ -30,7 +32,8 @@ class TestHmpSitesModel(BaseTestCase):
         partial_hmp = dict(TEST_HMP)
         partial_hmp.pop('gut', None)
         hmp_sites = HmpSitesResult(**partial_hmp)
-        sample = Sample(name='SMPL_01', hmp_sites=hmp_sites).save()
+        args = {'name': 'SMPL_01', MODULE_NAME: hmp_sites}
+        sample = Sample(**args).save()
         self.assertTrue(sample.hmp_sites)
         tool_result = sample.hmp_sites
         self.assertEqual(len(tool_result), 5)
@@ -40,10 +43,11 @@ class TestHmpSitesModel(BaseTestCase):
         self.assertEqual(tool_result['urogenital'], 0.7)
         self.assertEqual(tool_result['airways'], 0.1)
 
-    def test_add_malformed_hmp_sites_result(self):      # pylint: disable=invalid-name
+    def test_add_malformed_hmp_sites_result(self):  # pylint: disable=invalid-name
         """Ensure validation fails for value outside of [0,1]."""
         bad_hmp = dict(TEST_HMP)
         bad_hmp['gut'] = 1.5
         hmp_sites = HmpSitesResult(**bad_hmp)
-        sample = Sample(name='SMPL_01', hmp_sites=hmp_sites)
+        args = {'name': 'SMPL_01', MODULE_NAME: hmp_sites}
+        sample = Sample(**args).save()
         self.assertRaises(ValidationError, sample.save)
