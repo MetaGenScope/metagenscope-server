c17b74f33f1f7b764f833a198deade9047f0fc29
David Danko <dcdanko@gmail.com>
2018-04-16 13:02:23 -0400

    cped microbe_directory to start ancestry

---
+++ app/tool_results/ancestry/tests/test_model.py
@@ -0,0 +1,19 @@
+"""Test suite for Microbe Directory tool result model."""
+
+from app.samples.sample_models import Sample
+from app.tool_results.microbe_directory import MicrobeDirectoryToolResult
+
+from tests.base import BaseTestCase
+
+from .constants import TEST_DIRECTORY
+
+
+class TestMicrobeDirectoryModel(BaseTestCase):
+    """Test suite for Microbe Directory tool result model."""
+
+    def test_add_microbe_directory(self):
+        """Ensure Microbe Directory result model is created correctly."""
+
+        microbe_directory = MicrobeDirectoryToolResult(**TEST_DIRECTORY)
+        sample = Sample(name='SMPL_01', microbe_directory_annotate=microbe_directory).save()
+        self.assertTrue(sample.microbe_directory_annotate)
