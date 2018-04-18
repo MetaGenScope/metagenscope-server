82e034c6db8e5764367998e37b0957cdf0a77702
David Danko <dcdanko@gmail.com>
2018-04-18 23:16:19 +0200

    test suite

---
+++ app/display_modules/reads_classified/models.py
@@ -0,0 +1,19 @@
+"""Reads Classified display models."""
+
+from app.extensions import mongoDB as mdb
+
+
+class SingleReadsClassifiedResult(mdb.EmbeddedDocument):  # pylint: disable=too-few-public-methods
+    """Reads Classified for one sample"""
+
+    viral = mdb.IntField(required=True, default=0)
+    archaea = mdb.IntField(required=True, default=0)
+    bacteria = mdb.IntField(required=True, default=0)
+    host = mdb.IntField(required=True, default=0)
+    unknown = mdb.IntField(required=True, default=0)
+
+
+class ReadClassifiedResult(mdb.EmbeddedDocument):  # pylint: disable=too-few-public-methods
+    """Read stats embedded result."""
+
+    samples = mdb.MapField(field=SingleReadsClassifiedResult, required=True)
