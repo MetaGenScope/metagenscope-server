124c506f5225417a7e4c9a581b4bcd1537497559
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-04-19 12:22:05 -0400

    Update tests.

--- tests/display_module/test_conductor.py
+++ tests/display_module/test_conductor.py
@@ -1,8 +1,6 @@
-"""Test suite for SampleConductor."""
+"""Test suite for DisplayModuleConductors."""
 
-from uuid import uuid4
-
-from app.display_modules.conductor import SampleConductor
+from app.display_modules.conductor import DisplayModuleConductor
 from app.display_modules.sample_similarity import SampleSimilarityDisplayModule
 from app.tool_results.kraken import KrakenResultModule
 from app.tool_results.metaphlan2 import Metaphlan2ResultModule
@@ -13,27 +11,36 @@ KRAKEN_NAME = KrakenResultModule.name()
 METAPHLAN2_NAME = Metaphlan2ResultModule.name()
 
 
-class TestSampleConductor(BaseTestCase):
+class TestDisplayModuleConductor(BaseTestCase):
     """Test suite for display module Conductor."""
 
     def test_downstream_modules(self):
         """Ensure downstream_modules is computed correctly."""
-        sample_id = str(uuid4())
-        conductor = SampleConductor(sample_id, KrakenResultModule)
+        conductor = DisplayModuleConductor(KrakenResultModule)
         self.assertIn(SampleSimilarityDisplayModule, conductor.downstream_modules)
 
     def test_get_valid_modules(self):
         """Ensure valid_modules is computed correctly."""
         tools_present = set([KRAKEN_NAME, METAPHLAN2_NAME])
-        sample_id = str(uuid4())
-        conductor = SampleConductor(sample_id, KrakenResultModule)
+        conductor = DisplayModuleConductor(KrakenResultModule)
         valid_modules = conductor.get_valid_modules(tools_present)
         self.assertIn(SampleSimilarityDisplayModule, valid_modules)
 
     def test_partial_valid_modules(self):
         """Ensure valid_modules is computed correctly if tools are missing."""
         tools_present = set([KRAKEN_NAME])
-        sample_id = str(uuid4())
-        conductor = SampleConductor(sample_id, KrakenResultModule)
+        conductor = DisplayModuleConductor(KrakenResultModule)
         valid_modules = conductor.get_valid_modules(tools_present)
         self.assertTrue(SampleSimilarityDisplayModule not in valid_modules)
+
+
+class TestSampleConductor(BaseTestCase):
+    """Test suite for display module Conductor."""
+
+    pass
+
+
+class TestGroupConductor(BaseTestCase):
+    """Test suite for display module Conductor."""
+
+    pass
