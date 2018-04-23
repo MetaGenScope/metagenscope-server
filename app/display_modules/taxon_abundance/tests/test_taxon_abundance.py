289b04d9fe31cdc0b02354934d0eef4926fff635
David Danko <dcdanko@gmail.com>
2018-04-23 17:16:10 +0200

    tests 5

--- app/display_modules/taxon_abundance/tests/test_taxon_abundance.py
+++ app/display_modules/taxon_abundance/tests/test_taxon_abundance.py
@@ -1,9 +1,8 @@
 """Test suite for Taxon Abundance model."""
 
-from mongoengine import ValidationError
-
 from app.analysis_results.analysis_result_models import AnalysisResultMeta, AnalysisResultWrapper
 from app.display_modules.taxon_abundance import TaxonAbundanceResult
+from app.display_modules.taxon_abundance.constants import MODULE_NAME
 from app.display_modules.taxon_abundance.wrangler import TaxonAbundanceWrangler
 from app.samples.sample_models import Sample
 from app.tool_results.kraken.tests.factory import create_taxa
@@ -45,20 +44,20 @@ class TestTaxonAbundanceResult(BaseTestCase):
         self.assertTrue(result.id)
         self.assertTrue(result.taxon_abundance)
 
-    def test_get_read_stats(self):
+    def test_get_taxon_abundance(self):
         """Ensure getting a single TaxonAbundance behaves correctly."""
         taxon_abundance = TaxonAbundanceResult(kraken=flow_model(), metaphlan2=flow_model())
         self.generic_getter_test(taxon_abundance, MODULE_NAME)
 
-    def test_run_read_stats_sample_group(self):  # pylint: disable=invalid-name
+    def test_run_taxon_abundance_sample_group(self):  # pylint: disable=invalid-name
         """Ensure TaxonAbundance run_sample_group produces correct results."""
 
         def create_sample(i):
             """Create unique sample for index i."""
             return Sample(name=f'Sample{i}',
                           metadata={'foobar': f'baz{i}'},
-                          kraken=create_taxa(),
-                          metaphlan2=create_taxa()).save()
+                          kraken=create_taxa(100),
+                          metaphlan2=create_taxa(100)).save()
 
         self.generic_run_group_test(create_sample,
                                     TaxonAbundanceWrangler,
