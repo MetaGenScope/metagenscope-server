3189c7379c48775349516dc4735291ab2f2e3ffd
David Danko <dcdanko@gmail.com>
2018-04-23 17:08:27 +0200

    tests 4

--- app/display_modules/taxon_abundance/tests/test_taxon_abundance.py
+++ app/display_modules/taxon_abundance/tests/test_taxon_abundance.py
@@ -4,6 +4,9 @@ from mongoengine import ValidationError
 
 from app.analysis_results.analysis_result_models import AnalysisResultMeta, AnalysisResultWrapper
 from app.display_modules.taxon_abundance import TaxonAbundanceResult
+from app.display_modules.taxon_abundance.wrangler import TaxonAbundanceWrangler
+from app.samples.sample_models import Sample
+from app.tool_results.kraken.tests.factory import create_taxa
 from tests.base import BaseTestCase
 
 
@@ -42,9 +45,21 @@ class TestTaxonAbundanceResult(BaseTestCase):
         self.assertTrue(result.id)
         self.assertTrue(result.taxon_abundance)
 
-    def test_add_missing_node(self):
-        """Ensure saving model fails if edge references missing node."""
+    def test_get_read_stats(self):
+        """Ensure getting a single TaxonAbundance behaves correctly."""
         taxon_abundance = TaxonAbundanceResult(kraken=flow_model(), metaphlan2=flow_model())
-        wrapper = AnalysisResultWrapper(data=taxon_abundance)
-        result = AnalysisResultMeta(taxon_abundance=wrapper)
-        self.assertRaises(ValidationError, result.save)
+        self.generic_getter_test(taxon_abundance, MODULE_NAME)
+
+    def test_run_read_stats_sample_group(self):  # pylint: disable=invalid-name
+        """Ensure TaxonAbundance run_sample_group produces correct results."""
+
+        def create_sample(i):
+            """Create unique sample for index i."""
+            return Sample(name=f'Sample{i}',
+                          metadata={'foobar': f'baz{i}'},
+                          kraken=create_taxa(),
+                          metaphlan2=create_taxa()).save()
+
+        self.generic_run_group_test(create_sample,
+                                    TaxonAbundanceWrangler,
+                                    MODULE_NAME)
