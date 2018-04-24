55cee9d68c33f60cd499646c635eff00ca8ccb75
David Danko <dcdanko@gmail.com>
2018-04-24 12:05:04 +0200

    display modules for macrobes

---
+++ app/display_modules/macrobes/tests/factory.py
@@ -0,0 +1,25 @@
+# pylint: disable=missing-docstring,too-few-public-methods
+
+"""Factory for generating Macrobe models for testing."""
+
+import factory
+
+from app.display_modules.macrobes import MacrobeResult
+from app.tool_results.macrobes.tests.factory import create_values
+
+
+class MacrobeFactory(factory.mongoengine.MongoEngineFactory):
+    """Factory for Analysis Result's Macrobe."""
+
+    class Meta:
+        """Factory metadata."""
+
+        model = MacrobeResult
+
+    @factory.lazy_attribute
+    def samples(self):  # pylint: disable=no-self-use
+        """Generate random samples."""
+        samples = {}
+        for i in range(10):
+            samples[f'Sample{i}'] = create_values()
+        return samples
---
+++ app/display_modules/macrobes/wrangler.py
@@ -0,0 +1,44 @@
+"""Wrangler for Macrobe Directory results."""
+
+from celery import chain
+
+from app.display_modules.display_wrangler import DisplayModuleWrangler
+from app.display_modules.utils import persist_result_helper
+from app.extensions import celery
+from app.tool_results.macrobes import MacrobeResultModule
+
+from .constants import MODULE_NAME
+from .models import MacrobeResult
+
+
+@celery.task()
+def collate_macrobes(samples):
+    """Group a macrobes from a set of samples."""
+    sample_dict = {}
+    for sample in samples:
+        sample_name = sample['name']
+        sample_dict[sample_name] = sample[MacrobeResultModule.name()]['macrobes']
+    sample_tbl = DataFrame.from_dict(sample_dict, orient='index').fillna(0)
+    return {'samples': sample_tbl.to_dict()}
+
+
+@celery.task(name='macrobe_abundance.persist_result')
+def persist_result(result_data, analysis_result_id, result_name):
+    """Persist Macrone results."""
+    result = MacrobeResult(**result_data)
+    persist_result_helper(result, analysis_result_id, result_name)
+
+
+class MicrobeDirectoryWrangler(DisplayModuleWrangler):
+    """Tasks for generating virulence results."""
+
+    @classmethod
+    def run_sample_group(cls, sample_group, samples):
+        """Gather and process samples."""
+        collate_task = collate_macrobes.s(samples)
+        persist_task = persist_result.s(sample_group.analysis_result_uuid, MODULE_NAME)
+
+        task_chain = chain(collate_task, persist_task)
+        result = task_chain.delay()
+
+        return result
