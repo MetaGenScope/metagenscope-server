58b032ed43a3d3562c6cc34730dc92786eec8b68
David Danko <dcdanko@gmail.com>
2018-04-25 01:18:39 +0200

    single sample for pathways

--- app/display_modules/pathways/wrangler.py
+++ app/display_modules/pathways/wrangler.py
@@ -3,13 +3,26 @@
 from celery import chain
 
 from app.display_modules.display_wrangler import DisplayModuleWrangler
+from app.display_modules.utils import jsonify
 
 from .constants import MODULE_NAME
 from .tasks import filter_humann2_pathways, persist_result
 
 
 class PathwayWrangler(DisplayModuleWrangler):
-    """Task for generating Reads Classified results."""
+    """Task for generating Pathway results."""
+
+    @classmethod
+    def run_sample(cls, sample_id, sample):
+        """Gather single sample and process."""
+        samples = [jsonify(sample)]
+        persist_task = persist_result.s(sample.analysis_result.pk,
+                                        MODULE_NAME)
+        task_chain = chain(filter_humann2_pathways.s(samples),
+                           persist_task)
+        result = task_chain.delay()
+
+        return result
 
     @classmethod
     def run_sample_group(cls, sample_group, samples):
