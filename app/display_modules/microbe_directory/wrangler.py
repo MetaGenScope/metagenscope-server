e131388e7084cb9f4f8419a8e1c5e29ff6c58657
David Danko <dcdanko@gmail.com>
2018-04-25 01:26:39 +0200

    single sample for microbe directory

--- app/display_modules/microbe_directory/wrangler.py
+++ app/display_modules/microbe_directory/wrangler.py
@@ -3,7 +3,7 @@
 from celery import chain
 
 from app.display_modules.display_wrangler import DisplayModuleWrangler
-from app.display_modules.utils import collate_samples
+from app.display_modules.utils import jsonify, collate_samples
 from app.tool_results.microbe_directory import (
     MicrobeDirectoryToolResult,
     MicrobeDirectoryResultModule,
@@ -15,6 +15,22 @@ from .tasks import microbe_directory_reducer, persist_result
 
 class MicrobeDirectoryWrangler(DisplayModuleWrangler):
     """Tasks for generating virulence results."""
+
+    @classmethod
+    def run_sample(cls, sample_id, sample):
+        """Gather single sample and process."""
+        tool_result_name = MicrobeDirectoryResultModule.name()
+        samples = [jsonify(sample)]
+        collate_fields = list(MicrobeDirectoryToolResult._fields.keys())
+        collate_task = collate_samples.s(tool_result_name, collate_fields, samples)
+        reducer_task = microbe_directory_reducer.s()
+        persist_task = persist_result.s(sample.analysis_result.pk,
+                                        MODULE_NAME)
+
+        task_chain = chain(collate_task, reducer_task, persist_task)
+        result = task_chain.delay()
+
+        return result
 
     @classmethod
     def run_sample_group(cls, sample_group, samples):
