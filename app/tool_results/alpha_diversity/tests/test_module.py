8c14da4fc6ab2885976fa12844c80b47c5dec209
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-04-26 10:57:03 -0400

    Fix tool result tests.

--- app/tool_results/alpha_diversity/tests/test_module.py
+++ app/tool_results/alpha_diversity/tests/test_module.py
@@ -12,8 +12,8 @@ class TestAlphaDivModel(BaseToolResultTest):
     def test_add_alpha_div(self):
         """Ensure Alpha Div tool result model is created correctly."""
 
-        adivs = AlphaDiversityResultModule.result_model()(**create_values())
-        self.generic_add_sample_tool_test(adivs, AlphaDiversityResultModule.name())
+        alpha_diversity = AlphaDiversityResultModule.result_model()(**create_values())
+        self.generic_add_test(alpha_diversity, AlphaDiversityResultModule.name())
 
     def test_upload_alpha_div(self):
         """Ensure a raw Alpha Div tool result can be uploaded."""
