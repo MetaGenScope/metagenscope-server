f87f297cb1a93dd5e641b4f0458c401f0bf79c73
David Danko <dcdanko@gmail.com>
2018-04-27 17:31:38 -0400

    changed quotes for linting

--- app/tool_results/microbe_directory/tests/factory.py
+++ app/tool_results/microbe_directory/tests/factory.py
@@ -8,50 +8,50 @@ from app.tool_results.microbe_directory import MicrobeDirectoryToolResult
 def create_values():
     """Create microbe directory values."""
     return {
-        "gram_stain": {
-            "gram_positive": random(),
-            "gram_negative": random(),
-            "unknown": random(),
+        'gram_stain': {
+            'gram_positive': random(),
+            'gram_negative': random(),
+            'unknown': random(),
         },
-        "microbiome_location": {
-            "human": random(),
-            "non_human": random(),
-            "unknown": random(),
+        'microbiome_location': {
+            'human': random(),
+            'non_human': random(),
+            'unknown': random(),
         },
-        "antimicrobial_susceptibility": {
-            "known_abx": random(),
-            "unknown": random(),
+        'antimicrobial_susceptibility': {
+            'known_abx': random(),
+            'unknown': random(),
         },
-        "optimal_temperature": {
-            "37c": random(),
-            "unknown": random(),
+        'optimal_temperature': {
+            '37c': random(),
+            'unknown': random(),
         },
-        "extreme_environment": {
-            "mesophile": random(),
-            "unknown": random(),
+        'extreme_environment': {
+            'mesophile': random(),
+            'unknown': random(),
         },
-        "biofilm_forming": {
-            "yes": random(),
-            "unknown": random(),
+        'biofilm_forming': {
+            'yes': random(),
+            'unknown': random(),
         },
-        "optimal_ph": {
-            "unknown": random(),
+        'optimal_ph': {
+            'unknown': random(),
         },
-        "animal_pathogen": {
-            "unknown": random(),
+        'animal_pathogen': {
+            'unknown': random(),
         },
-        "spore_forming": {
-            "no": random(),
-            "unknown": random(),
+        'spore_forming': {
+            'no': random(),
+            'unknown': random(),
         },
-        "pathogenicity": {
-            "cogem_1": random(),
-            "cogem_2": random(),
-            "unknown": random(),
+        'pathogenicity': {
+            'cogem_1': random(),
+            'cogem_2': random(),
+            'unknown': random(),
         },
-        "plant_pathogen": {
-            "no": random(),
-            "unknown": random(),
+        'plant_pathogen': {
+            'no': random(),
+            'unknown': random(),
         }
     }
 
