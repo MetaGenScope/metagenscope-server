568533c1e70d322c1fae378442d09f6c265c5c69
David Danko <dcdanko@gmail.com>
2018-04-28 13:06:48 -0400

    fill in potential microbe directory blanks in wrangler

--- app/display_modules/microbe_directory/tasks.py
+++ app/display_modules/microbe_directory/tasks.py
@@ -1,9 +1,41 @@
 """Tasks for generating Microbe Directory results."""
 
+from pandas import DataFrame
+
 from app.extensions import celery
 from app.display_modules.utils import persist_result_helper
+from app.tool_results.microbe_directory import (
+    MicrobeDirectoryToolResult,
+    MicrobeDirectoryResultModule,
+)
 
 from .models import MicrobeDirectoryResult
+
+
+@celery.task()
+def collate_microbe_directory(samples):
+    """Collate a group of microbe directory results and fill in blanks."""
+    tool_name = MicrobeDirectoryResultModule.name()
+    fields = list(MicrobeDirectoryToolResult._fields.keys())
+    field_dict = {}
+    for field in fields:
+        field_dict[field] = {}
+        for sample in samples:
+            sample_name = sample['name']
+            tool_result = sample[tool_name]
+            field_dict[field][sample_name] = tool_result[field]
+        field_df = DataFrame.from_dict(field_dict[field])
+        field_df = field_df.fillna(0)
+        field_dict[field] = field_df.to_dict()
+
+    sample_dict = {}
+    for sample in samples:
+        sample_name = sample['name']
+        sample_dict[sample_name] = {}
+        for field in fields:
+            sample_dict[sample_name][field] = field_dict[field][sample_name]
+
+    return sample_dict
 
 
 @celery.task()
