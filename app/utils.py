9b7cf6419b0105fb1682544e3f2634849d9ccd28
David Danko <dcdanko@gmail.com>
2018-05-01 07:55:44 -0400

    linting 2

--- app/utils.py
+++ app/utils.py
@@ -6,8 +6,10 @@ from functools import wraps
 def lock_function(lock):
     """Lock a function but always release that lock."""
     def decorator(func):
+        """Lock a function but always release that lock."""
         @wraps(func)
         def wrapper(*args, **kwargs):
+            """Lock a function but always release that lock."""
             try:
                 lock.acquire()
                 return func(*args, **kwargs)
