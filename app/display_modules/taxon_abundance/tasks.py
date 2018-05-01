9a56f372297c8af8a81477503cede0fd8c734ede
David Danko <dcdanko@gmail.com>
2018-05-01 14:06:18 -0400

    return tables as dict taxa abund

--- app/display_modules/taxon_abundance/tasks.py
+++ app/display_modules/taxon_abundance/tasks.py
@@ -101,7 +101,7 @@ def make_taxa_table(samples, tool_name):
     taxa_tbl = pd.DataFrame.from_dict(taxa_tbl, orient='index')
     taxa_tbl = taxa_tbl.apply(lambda col: col / col.sum(), axis=0)
 
-    return taxa_tbl
+    return taxa_tbl.to_dict(orient='index')
 
 
 @celery.task()
