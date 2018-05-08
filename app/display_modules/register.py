26483a14e4e39e1d3341fbb1bc252ad8eb040e0f
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-05-08 15:52:48 -0400

    Fix argument naming as per code review.

--- app/display_modules/register.py
+++ app/display_modules/register.py
@@ -11,7 +11,7 @@ from app.api.exceptions import InvalidRequest
 from .utils import jsonify
 
 
-def get_result(cls, result_uuid):
+def get_result(display_module, result_uuid):
     """Define handler for API requests that defers to display module type."""
     try:
         uuid = UUID(result_uuid)
@@ -21,12 +21,12 @@ def get_result(cls, result_uuid):
     except DoesNotExist:
         raise NotFound('Analysis Result does not exist.')
 
-    if cls.name() not in analysis_result:
-        raise InvalidRequest(f'{cls.name()} is not in this AnalysisResult.')
+    if display_module.name() not in analysis_result:
+        raise InvalidRequest(f'{display_module.name()} is not in this AnalysisResult.')
 
-    module_results = getattr(analysis_result, cls.name()).fetch()
-    result = cls.get_data(module_results)
-    for transmission_hook in cls.transmission_hooks():
+    module_results = getattr(analysis_result, display_module.name()).fetch()
+    result = display_module.get_data(module_results)
+    for transmission_hook in display_module.transmission_hooks():
         result = transmission_hook(result)
 
     # Conversion to dict is necessary to avoid object not callable TypeError
@@ -34,14 +34,14 @@ def get_result(cls, result_uuid):
     return result_dict, 200
 
 
-def register_display_module(cls, router):
+def register_display_module(display_module, router):
     """Register API endpoint for this display module type."""
-    endpoint_url = f'/analysis_results/<result_uuid>/{cls.name()}'
-    endpoint_name = f'get_{cls.name()}'
+    endpoint_url = f'/analysis_results/<result_uuid>/{display_module.name()}'
+    endpoint_name = f'get_{display_module.name()}'
 
     def view_function(result_uuid):
         """Wrap get_result to provide class."""
-        return get_result(cls, result_uuid)
+        return get_result(display_module, result_uuid)
 
     router.add_url_rule(endpoint_url,
                         endpoint_name,
